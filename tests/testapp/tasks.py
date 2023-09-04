31a1229207997420582948fab4def06e61c9f8b3
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-09-04 16:37:57 +0200

    [tests] fixed test task

--- tests/testapp/tasks.py
+++ tests/testapp/tasks.py
@@ -4,13 +4,13 @@ from celery import shared_task
 
 @shared_task(bind=True)
 def test_task(self, context):
-    self.add_note('NoteOne')
+    context.add_note('NoteOne')
 
     time.sleep(2)
-    self.add_note('NoteTwo')
+    context.add_note('NoteTwo')
 
     time.sleep(4)
-    self.add_note('NoteThree')
+    context.add_note('NoteThree')
 
     time.sleep(6)
-    self.add_note('NoteFour')
+    context.add_note('NoteFour')
