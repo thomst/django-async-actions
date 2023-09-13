7018ac7bc4d11788dc7b604a2b0b296c86a3cc3c
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-09-13 23:51:16 +0200

    [tests] skip the obsolete ModelAdmin

--- tests/testapp/admin.py
+++ tests/testapp/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from item_messages.actions import clear_item_messages
 from async_actions.actions import as_action
 from async_actions.admin import ActionTaskModelAdmin
-from async_actions.models import ObjectTaskState
 from .models import TestModel
 from .tasks import test_task
 from .tasks import task_with_arg
@@ -20,8 +19,3 @@ class TestModelAdmin(ActionTaskModelAdmin):
         as_action(task_that_fails),
         clear_item_messages,
         ]
-
-
-@admin.register(ObjectTaskState)
-class ObjectTaskStateAdmin(admin.ModelAdmin):
-    pass
