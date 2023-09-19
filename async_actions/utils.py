2475ad30da72ef4fffc1eec960762bb43ac3d3d5
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-09-19 19:23:45 +0200

    [utils] remove obsolete import-functions

--- async_actions/utils.py
+++ async_actions/utils.py
@@ -1,17 +1,3 @@
-import sys
-import importlib
-
-# We need to import ContentType dynamically because on celery app initialization
-# the django apps are not yet loaded.
-def import_content_type():
-    return dyn_import('django.contrib.contenttypes.models', 'ContentType')
-
-
-def dyn_import(module, attr):
-    if not hasattr(sys.modules[__name__], attr):
-        ct_module = importlib.import_module(module)
-        setattr(sys.modules[__name__], attr, getattr(ct_module, attr))
-    return getattr(sys.modules[__name__], attr)
 
 
 def get_message_checksum(task):
