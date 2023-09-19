064e777574475f36ccf4e7887517d07cac3bda50
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-09-19 20:26:11 +0200

    [messages] renaming task_state variable and template name

--- async_actions/messages.py
+++ async_actions/messages.py
@@ -5,22 +5,22 @@ from item_messages import add_message, clear_messages
 from item_messages import INFO, ERROR
 
 
-def build_task_message(result):
-    template = 'async_actions/action_task.html'
-    context = dict(result=result, meta=result.get_meta_data())
+def build_task_message(task_state):
+    template = 'async_actions/task_message.html'
+    context = dict(task_state=task_state)
     msg = render_to_string(template, context)
     return mark_safe(msg)
 
 
-def add_task_message(request, result):
-    msg = build_task_message(result)
-    if result.status == states.FAILURE:
-        add_message(request, ERROR, result.obj, msg)
+def add_task_message(request, task_state):
+    msg = build_task_message(task_state)
+    if task_state.status == states.FAILURE:
+        add_message(request, ERROR, task_state.obj, msg)
     else:
-        add_message(request, INFO, result.obj, msg)
+        add_message(request, INFO, task_state.obj, msg)
     return msg
 
 
-def set_task_message(request, result):
-    clear_messages(request, result.obj)
-    return add_task_message(request, result)
+def set_task_message(request, task_state):
+    clear_messages(request, task_state.obj)
+    return add_task_message(request, task_state)
