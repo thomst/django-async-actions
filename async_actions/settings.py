a8aba207d968b66204132389e0c68930b692496f
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-10-12 20:53:53 +0200

    [settings] added ASYNC_ACTION_DEBUG_MESSAGES setting

--- async_actions/settings.py
+++ async_actions/settings.py
@@ -5,3 +5,4 @@ from .processor import Processor
 
 
 ASYNC_ACTIONS_PROCESSOR_CLS = getattr(settings, 'ASYNC_ACTIONS_PROCESSOR_CLS', Processor)
+ASYNC_ACTION_DEBUG_MESSAGES = getattr(settings, 'ASYNC_ACTION_DEBUG_MESSAGES', False)
