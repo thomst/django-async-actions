237790b2a1fa506ce87fd5c9ef8e5650f0c6d365
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-10-12 09:43:26 +0200

    use custom css for ActionTaskModelAdmin

--- async_actions/admin.py
+++ async_actions/admin.py
@@ -5,6 +5,9 @@ from .models import ActionTaskState
 class ActionTaskModelAdmin(admin.ModelAdmin):
     class Media:
         js = ["admin/js/task-monitoring.js"]
+        css = {
+             'all': ('admin/css/task-messages.css',)
+        }
 
 
 @admin.register(ActionTaskState)
