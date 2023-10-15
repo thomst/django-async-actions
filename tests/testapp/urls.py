51916426ef208ed950e4cbd83d064efdb9845d1b
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-10-15 17:45:59 +0200

    [tests] some code cleanup in urls.py

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('async_actions/', (include("async_actions.urls"))),
+    path('async_actions/', include("async_actions.urls")),
 ]
